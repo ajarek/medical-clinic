945bf4583e5e78e44bc530645b98293b5787dc76
ajarek <ajarek@poczta.onet.pl>
2021-07-16 00:11:27 +0200

    sending the code

---
+++ login.js
@@ -0,0 +1,32 @@
+const btn = document.querySelector('.btn')
+const email = document.querySelector('#email')
+const pass = document.querySelector('#pass')
+const alert =document.querySelector('.alert')
+ 
+const sprawdz = (e) => {
+    e.preventDefault()
+    localStorageUser = JSON.parse(localStorage.getItem('data'))
+    if (localStorageUser === null) {
+        data = []
+    }
+     else {
+        for (i = 0; i < localStorageUser.length; i++) {
+            if(localStorageUser[i].emailUser===email.value && localStorageUser[i].passwordUser===pass.value){
+               
+                window.location.replace('doctors.html')
+                
+            }
+            else{
+                alert.innerHTML=`<i class="fas fa-exclamation-triangle"></i> złe dane!`
+        alert.style.color='red'
+        
+            }
+            
+        }
+
+    } 
+   
+}
+
+
+btn.addEventListener('click', sprawdz)
---
+++ register.js
@@ -0,0 +1,46 @@
+const btn = document.querySelector('.btn')
+const fullName = document.querySelector('#name')
+const email = document.querySelector('#email')
+const password = document.querySelector('#pass')
+const passConfirm = document.querySelector('#passConfirm')
+const alert=document.querySelector('main .p')
+let data
+
+let arr = []
+const odczytaj = (e) => {
+    
+    e.preventDefault()
+
+
+
+   
+saveStorage()
+}
+
+const saveStorage = () => {
+    const localStorageData = localStorage.getItem('data')
+    let data
+    if (localStorageData === null) {
+        data = []
+    } else {
+        data = JSON.parse(localStorageData)
+    }
+    if (fullName.value != '' && email.value != '' && password.value != '' && passConfirm.value != ''&& password.value==passConfirm.value) {
+        let user = {
+            nameUser: fullName.value,
+            emailUser: email.value,
+            passwordUser: password.value
+        }
+        data.push(user)
+        alert.innerHTML=`<i class="fas fa-exclamation-circle"></i> dodano!`
+        alert.style.color='green'
+    }
+    else{
+        alert.innerHTML=`<i class="fas fa-exclamation-triangle"></i> złe dane!`
+        alert.style.color='red'
+    }
+    localStorage.setItem('data', JSON.stringify(data))
+    fullName.value ='' ; email.value =''; password.value ='' ; passConfirm.value =''
+}
+
+btn.addEventListener('click', odczytaj)
